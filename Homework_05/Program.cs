6acc83d494b4cf1459c806ea7965f8ebbddd2f70
Forofonof <DanielResult1@outlook.com>
2022-04-01 19:36:00 +0300

    Добавьте файлы проекта.

---
+++ Homework_05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        int gold;
+        int crystalPrice = 15;
+        int crystalCount;
+
+        Console.WriteLine("У вас новое задание: нужно обменять золотые монеты на кристалы, сколько возьмете золотых монет?");
+        Console.WriteLine("Введите количество золотых монет:");
+        gold = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Вы получили: {gold} золотых монет.");
+        Console.WriteLine($"Добро пожаловать в forofonof.bank! Курс обмена: 1 кристал = {crystalPrice} золотых монет.");
+        Console.WriteLine("Сколько кристалов желаете приобрети?");
+        crystalCount = Convert.ToInt32(Console.ReadLine());
+        gold -= crystalPrice * crystalCount;
+        Console.WriteLine("Сделка успешно завершена!");
+        Console.WriteLine($"Вы получили: {crystalCount} кристалов.");
+        Console.WriteLine($"У вас осталось: {gold} золотых монет. {crystalCount} кристалов.");
+    }
+}
